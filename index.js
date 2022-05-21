9828259a8b3923572e01c5e1866606f808ec9c47
ian <ian.murphy92@gmail.com>
2022-05-21 15:28:52 -0400

    commit tree d906742cdf49c9814a995026a605b6c6bae51886

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+// makes everything match by making it all lowercase
+ function findMatching(drivers, name) {
+
+     return drivers.filter((driver) => driver.toLowerCase() === name.toLowerCase())
+
+ }
+
+function fuzzyMatch(drivers, init){
+    return drivers.filter((driver) => driver.startsWith(init))
+}
+
+function matchName(drivers, names){
+    return drivers.filter(driver => driver.name === names)
+}
+
